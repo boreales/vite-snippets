477e8903edb5a7eb9526895438214747cdaec1e5
Josselin Dionisi <dionisi.josselin@gmail.com>
2025-07-15 09:43:19 +0200

    Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4Nm41gN8_-7OQ0oz3PQJJbhtpyfyBn_I",
-    authDomain: "react-app-74ec9.firebaseapp.com",
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
     databaseURL: "https://react-app-74ec9-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "react-app-74ec9",
-    storageBucket: "react-app-74ec9.firebasestorage.app",
-    messagingSenderId: "692377415770",
-    appId: "1:692377415770:web:edd0ca7d8cc30223d76863"
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 const app = initializeApp(firebaseConfig);
