8d6e2606968b69cdd94511b6caf8d411d01584b4
Josselin Dionisi <dionisi.josselin@gmail.com>
2025-07-18 08:42:22 +0200

    Add route

--- src/SnippetPage.jsx
+++ src/SnippetPage.jsx
@@ -2,12 +2,17 @@ import { useParams } from 'react-router-dom';
 
 export default function SnippetPage({ snippets }) {
   const { id } = useParams();
-  const snippet = snippets[id];
+  const snippet = snippets.find((snippet) => {
+    if (snippet.id.toString() === id.toString()) {
+      return snippet;
+    }
+  });
   if (!snippet) return <div>Snippet introuvable</div>;
   return (
     <div>
       <h2>{snippet.title}</h2>
       <pre>{snippet.code}</pre>
+      <a href="/">Retour à la liste des snippets</a>
     </div>
   );
 }
